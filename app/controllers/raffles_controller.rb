d054897cc2321deb904570e74c5ab6eb42176f17
juuhgouvea <juliasantos752@yahoo.com.br>
2021-03-28 18:15:30 -0300

    add callback to create users and kinds

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -1,5 +1,7 @@
 class RafflesController < ApplicationController
   before_action :set_raffle, only: [:show, :edit, :update, :destroy]
+  before_action :create_users, only: [:new, :edit]
+  before_action :create_kinds, only: [:new, :edit]
 
   # GET /raffles
   def index
@@ -54,5 +56,26 @@ class RafflesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def raffle_params
       params.require(:raffle).permit(:title, :description, :initial_draw_date, :start_sale_date, :end_sale_date, :draw_date, :ticket_price, :user_id, :kind_id)
+    end
+
+    def create_users
+      return true if User.all.any?
+
+      User.create(
+        [
+          { name: 'Admin', email: 'admin@email.com', password: 'password', is_admin: true },
+          { name: 'User', email: 'user@email.com', password: 'password', is_admin: false }
+        ]
+      )
+    end
+
+    def create_kinds
+      return true if Kind.all.any?
+      Kind.create(
+        [
+          { description: 'Numeros de 1 a 100, de 1 em 1', initial_number: 1, step: 1, tickets_amount: 100 },
+          { description: 'Numeros de 5 a 200, de 5 em 5', initial_number: 5, step: 5, tickets_amount: 40 }
+        ]
+      )
     end
 end
