99dab67bc628a3bc456bbb8c2600a04c8bbf4afb
juuhgouvea <juliasantos752@yahoo.com.br>
2021-04-05 00:40:37 -0300

    generate users and kinds using database seeder

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(
+  [
+    { name: 'Admin', email: 'admin@email.com', password: 'password', is_admin: true },
+    { name: 'User', email: 'user@email.com', password: 'password', is_admin: false }
+  ]
+)
+
+Kind.create(
+  [
+    {
+      description: 'Gerar 1.000 bilhetes, numeros de 1 a 1.000 de 1 em 1',
+      initial_number: 1,
+      step: 1,
+      tickets_amount: 100
+    }
+  ]
+)
